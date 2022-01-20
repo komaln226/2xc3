a0bda272d9686532ea077676e3b944b86815dd96
komaln226 <naeemk2@mcmaster.ca>
2022-01-20 18:23:42 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,7 @@
-
+def are_valid_group(studentNumber, groups):
+    result = False
+    for x in studentNumber:
+        for y in groups:
+            if x == y:
+                result = True 
+                return result 
