9e8a429fbcce6c4a1756921a3358957436ac33b5
komaln226 <naeemk2@mcmaster.ca>
2022-01-21 12:31:45 -0500

    komal edit

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
 def are_valid_groups(student_numbers, groups):
     for student in student_numbers:
-        if not any(student in x for x in groups):
-            return False
-    return True
+        if not any(student in x groups):
+            return True
+    return False
