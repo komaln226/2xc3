1d6c221919872a4d01d9b6466274577109a4f750
Muzamil Janjua <Muzamil2002@hotmail.com>
2022-01-21 12:44:54 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,12 +1,5 @@
 def are_valid_groups(student_numbers, groups):
-    if not any((2 > len(x) > 3) for x in groups):
-        return False
-
     for student in student_numbers:
-        occ = 0
-        for group in groups:
-            occ += group.count(student)
-        if occ != 1:
+        if not any(student in x for x in groups):
             return False
-
     return True
