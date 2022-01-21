c75eca96a3017b6e8b9a6085df36eeb1babc8bb2
komaln226 <naeemk2@mcmaster.ca>
2022-01-21 13:11:55 -0500

    KOmal

--- code.py
+++ code.py
@@ -1,12 +1,12 @@
 def are_valid_groups(student_numbers, groups):
     if not any((2 > len(x) > 3) for x in groups):
-        return True
+        return False
 
-    for student_numbers in student:
+    for student in student_numbers:
         occ = 0
         for group in groups:
             occ += group.count(student)
-        if 0 < occ < 2:
+        if occ != 1:
             return False
 
     return True
