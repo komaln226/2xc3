091e4a76b7017b871ce24637b76fa19673847eab
Muzamil Janjua <Muzamil2002@hotmail.com>
2022-01-21 12:17:36 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,5 @@
-def are_valid_group(studentNumber, groups):
-    result = False
-    for x in studentNumber:
-        for y in groups:
-            if x == y:
-                result = True 
-                return result 
+def are_valid_groups(student_numbers, groups):
+    for student in student_numbers:
+        if not any(student in x for x in groups):
+            return False
+    return True
