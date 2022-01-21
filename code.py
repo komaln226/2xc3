83afb53eaea0cc6d3156e03aac9212d843f924c4
komaln226 <naeemk2@mcmaster.ca>
2022-01-21 12:15:32 -0500

    Komal Code

--- code.py
+++ code.py
@@ -1,5 +1,7 @@
-def are_valid_groups(student_numbers, groups):
-    for student in student_numbers:
-        if not any(student in x for x in groups):
-            return False
-    return True
+def are_valid_group(studentNumber, groups):
+    result = False
+    for x in studentNumber:
+        for y in groups:
+            if x == y:
+                result = True 
+                return result 
