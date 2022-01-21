526850607523e0cac9790c34b75ff15198c05c77
Farzan Faisal <farzanalif42@gmail.com>
2022-01-21 12:33:23 -0500

    Farzan's edit

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 def are_valid_groups(student_numbers, groups):
-    for student in student_numbers:
+    for studentid in student_numbers:
         if not any(student in x groups):
             return True
-    return False
+        else:
+            return False
