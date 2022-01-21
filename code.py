f93f184636f2cfd2f045be397ee8f99a75dbb62a
Farzan Faisal <farzanalif42@gmail.com>
2022-01-21 13:08:14 -0500

    Farzan's edit

--- code.py
+++ code.py
@@ -1,8 +1,8 @@
 def are_valid_groups(student_numbers, groups):
     if not any((2 > len(x) > 3) for x in groups):
-        return False
+        return True
 
-    for student in student_numbers:
+    for student_numbers in student:
         occ = 0
         for group in groups:
             occ += group.count(student)
